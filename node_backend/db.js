791c07dd637c0cae75be36cfdea9d915db025ce7
aidan-lane <aidanlane7@gmail.com>
2020-12-17 19:54:49 -0500

    changed port conflict

--- node_backend/db.js
+++ node_backend/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
   host: 'gameserver_db_1',
-  port: 5432,
+  port: 5433,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB
--- node_backend/tests/users.js
+++ node_backend/tests/users.js
@@ -5,35 +5,50 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
 
-// Database
+// Database Connection Pool
 const { pool } = require('../db')
 
 chai.use(chaiHttp)
+/* 
+    Sets up tests for the users endpoint 
+    We are using Chai to setup and run unit tests
+    A test has a description (describe()) what it
+    does (it()) and then the actual execution.
+*/
 describe('User Tests', () => {
     beforeEach((done) => {
+        // Before each test, completely clear the users table
         const query = `DELETE FROM users`;
         pool.query(query, (err, res) => {
             done();
         });
     });
 
+    /* 
+        Testing POST for users
+        Tries to add a new user to the database
+    */
     describe('/POST Register', () => {
         it('should add new user to table', (done) => {
             chai.request(pool)
-                .post('/users')
-                .send({
+                .post('/users') // actual endpoints
+                .send({ // body data
                     username: 'name',
                     email: 'email@gmail.com',
                     password: 'pass123'
                 })
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.length.should.be.eql(1);
+                .end((err, res) => { // on receive
+                    res.should.have.status(200); // should be successful status
+                    res.body.length.should.be.eql(1);  // should return a single result
                     done();
                 });
         });
     });
 
+    /* 
+        Testing GET for users
+        Checks if a non-existing user exists in the table
+    */
     describe('/GET Login', () => {
         it('should check if user exists in table', (done) => {
             chai.request(pool)
@@ -43,13 +58,17 @@ describe('User Tests', () => {
                     password: 'pass123456'
                 })
                 .end((err, res) => {
-                    res.should.have.status(400);
-                    res.body.length.should.be.eql(0);
+                    res.should.have.status(400); // should be unsuccessful
+                    res.body.length.should.be.eql(0); // nothing is returned
                     done();
                 });
         });
     });
 
+    /*
+        Testing DELETE for users
+        Checks if a request to remove a non-exisisting user is made
+    */
     describe('/DELETE Remove', () => {
         it('should remove a user if they exist in table', (done) => {
             chai.request(pool)
